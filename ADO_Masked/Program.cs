15e2dbb3cc9c241ad05f42f1061112d6b04403aa
Zulfuqar Asgerzade <37780533+ZulfuqarAsgerzade@users.noreply.github.com>
2023-01-12 13:11:22 +0400

    v1

---
+++ ADO_Masked/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO_Masked {
+    public class Program
+    {
+        public static void Main()
+        {
+            SqlConnection sqlConnection = null;
+
+            try
+            {
+                sqlConnection = new SqlConnection("Server=localhost; Database=Classwork; Integrated Security=True;");
+
+                sqlConnection.Open();
+
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("execute as user = 'Exi'; Select * from Users", sqlConnection);
+
+                DataTable dataSet = new DataTable();
+
+                sqlDataAdapter.Fill(dataSet);
+
+
+
+
+                    foreach (DataColumn column in dataSet.Columns)
+                    {
+                        Console.WriteLine(column);
+
+                        foreach (DataRow row in dataSet.Rows)
+                        {
+                        Console.WriteLine(row[column]);
+                        }
+
+                    }
+
+
+
+
+
+
+
+                //while (sqlDataReader.Read())
+                //{
+                //    Console.WriteLine(
+                //        "Firstname: " + sqlDataReader["FirstName"] + "\n" +
+                //        "LastName: " + sqlDataReader["LastName"] + "\n" +
+                //        "CardNumber: " + sqlDataReader["CardNumber"] + "\n" +
+                //        new String('-', 50)
+                //        );
+                //}
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+
+        }
+    }
+}
+
